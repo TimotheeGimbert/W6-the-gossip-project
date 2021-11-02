8b32f25fc899df655eb71f2767891e2afef7e1be
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-02 15:25:54 +0100

    adds routes resources and creation gossips page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :gossips
+
   root 'page#index'
   get '/contact', to: 'page#contact'
   get '/team', to: 'page#team'
   get '/welcome/:input', to: 'page#welcome'
+  
 end
