42ff4ceb9165037c24ca4e96cdafec842d43caea
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-05 21:07:24 +0100

    header bootstrap responsive

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       log_in(user)
+      redirect_to root_path
     else
       flash.now[:danger] = 'Invalid user/pass combo !'
       render 'new'
